bde091c4358ab0ce9d8ac4253d6096251b7ad1d7
Kevin Zhang <kevinzhang3702@gmail.com>
2017-02-13 15:03:27 -0500

    did some more work... Waiting for ayman?

--- paddlin.js
+++ paddlin.js
@@ -1,11 +1,15 @@
 
-var b = document.getElementById("clr-btn");
-b.addEventListener( "onclick", function(){
-    var c = document.getElementById("paddledCanvas");
-    w = c.getAttribute("width");
-    h = c.getAttribute("height");
-    ctx = c.getContext("2d");
+var clear = function(){
+    var c = document.getElementById( "paddledCanvas" );
+    w = c.getAttribute( "width" );
+    h = c.getAttribute( "height" );
+    ctx = c.getContext( "2d" );
     ctx.clearRect( 0, 0, w, h);
-});
+}
 
-var c = document.getElementById("paddledCanvas");
+var draw = function( event ){
+    var c = document.getElementById( "paddledCanvas" );
+    ctx = c.getContext( "2d" );
+    ctx.fillStyle( "#8159af" );
+    ctx.fillRect(event.clientX - 10, event.clientY -10, 20, 20);
+}
